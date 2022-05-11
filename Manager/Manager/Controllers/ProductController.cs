7f1e8c73372729b8e2efb51c6df0ac409cb5d899
ADMIN\Admin <chienymhy@gmail.com>
2022-05-12 01:01:56 +0700

    first commit

---
+++ Manager/Manager/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+namespace Manager.Controllers
+{
+    public class ProductController : Controller
+    {
+        MyDbContext _db;
+        public ProductController(MyDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var product = _db.Products.ToList();
+            return View(product);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IFormFile file, Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName ="";
+                if (file != null)
+                {
+                     uniqueFileName = await UploadFile(file);
+                }
+
+                product.LinkImage = uniqueFileName;
+                product.ProductId = Guid.NewGuid();
+                var a = deletefile("2.jpg");
+                _db.Products.Add(product);
+                _db.SaveChanges();
+
+                //Employee employee = new Employee
+                //{
+                //    FirstName = model.FirstName,
+                //    LastName = model.LastName,
+                //    FullName = model.FirstName + " " + model.LastName,
+                //    Gender = model.Gender,
+                //    Age = model.Age,
+                //    Office = model.Office,
+                //    Position = model.Position,
+                //    Salary = model.Salary,
+                //    ProfilePicture = uniqueFileName,
+                //};
+                //dbContext.Add(employee);
+                //await dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        private async Task<string> UploadFile(IFormFile ufile)
+        {
+            if (ufile != null && ufile.Length > 0)
+            {
+                var fileName = Guid.NewGuid() + "-" + Path.GetFileName(ufile.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images", fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ufile.CopyToAsync(fileStream);
+                }
+                return fileName;
+            }
+            return "";
+        }
+        public bool deletefile(string fname)
+        {
+            string _imageToBeDeleted =  Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images", fname);
+            if ((System.IO.File.Exists(_imageToBeDeleted)))
+            {
+                System.IO.File.Delete(_imageToBeDeleted);
+            }
+            return true;
+        }
+    }
+}
---
+++ Manager/Manager/Migrations/20220511162514_addTableProduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Manager.Migrations
+{
+    public partial class addTableProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    ProductName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<double>(type: "double", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Discount = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LinkImage = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
